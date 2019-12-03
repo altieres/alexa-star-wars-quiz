1c4caf24a3b498537c97738b2a2a0b9da6338596
Altieres Lopes <altiereslopes@webgoal.com.br>
2019-12-03 11:26:25 -0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#  Skill Alexa de Perguntas sobre Star Wars
+
+Ensine à Alexa uma grande quantidade de questões de multipla escolha sobre os filmes de Star Wars.
+
---
+++ lambda/custom/questions.js
@@ -0,0 +1,228 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_PT_BR: [
+    {
+      'Em qual planeta Anakin luta contra Obi Wan Kenobi?': [
+        'Mustafar',
+        'Dagobah',
+        'Coruscant',
+        'Tattoine',
+      ]
+    }, {
+      'Qual o nome do antigo mestre de Obi Wan?': [
+        'Qui Gon Jinn',
+        'Mace Windu',
+        'Dookan',
+        'Yoda',
+      ]
+    }, {
+      'Qual é a cor do sabre dos Sith?': [
+        'Azul',
+        'Roxo',
+        'Verde',
+        'Vermelho',
+      ]
+    }, {
+      'O que Luke e Léia são?': [
+        'Irmãos',
+        'Amigos',
+        'Primos',
+        'Namorados',
+      ]
+    }, {
+      'Qual o nome do andróide que sempre aparece em todos os filmes?': [
+        'C-3PO',
+        'R2-D2',
+        'R2-PO',
+        'C-3D2',
+      ]
+    }, {
+      'Qual destes personagens é de Star Wars?': [
+        'Darth Sidious',
+        'Uhura',
+        'Kirk',
+        'Spock',
+      ]
+    }, {
+      'Quantos braços tinha o General Grievous?': [
+        '4 Braços',
+        '6 Braços',
+        '8 Braços',
+        '2 Braços',
+      ]
+    }, {
+      'Qual o nome da mãe de Luke e Léia?': [
+        'Padmé',
+        'Shmi',
+        'Cordé',
+        'Satine',
+      ]
+    }, {
+      'Qual é a frase mais famosa que Darth Vader disse a Luke?': [
+        'Não, eu sou seu pai',
+        'Não Luke, eu sou seu pai',
+        'Luke, eu sou seu pai',
+        'Luke, sou seu pai',
+      ]
+    }, {
+      'Qual a profissão de Boba Fett?': [
+        'Caçador de Recompensas',
+        'Mercador',
+        'General',
+        'Jedi',
+      ]
+    }, {
+      'Qual a relação entre Darth Vader e Anakin Skywalker?': [
+        'São a mesma pessoa',
+        'Pai e filho',
+        'Irmãos',
+        'Primos',
+      ]
+    }, {
+      'Qual destes filmes foi lançado primeiro?': [
+        'Episódio IV: Uma Nova Esperança',
+        'Episódio III: A Vingança dos Sith',
+        'Episódio VI: O Retorno de Jedi',
+        'Episódio I: A Ameaça Fantasma',
+      ]
+    }, {
+      'Qual destes filmes foi lançado por último?': [
+        'Episódio III: A Vingança dos Sith',
+        'Episódio IV: Uma Nova Esperança',
+        'Episódio VI: O Retorno de Jedi',
+        'Episódio I: A Ameaça Fantasma',
+      ]
+    }, {
+      'Em que ano o filme: Star Wars Episódio IV: Uma Nova Esperança foi lançado?': [
+        '1977',
+        '1989',
+        '1987',
+        '1978',
+      ]
+    }, {
+      'Qual é o Jedi que treinou Yoda?': [
+        'N ghata',
+        'Ayala secury',
+        'Luke Skywalker',
+        'Obi Wan',
+      ]
+    }, {
+      'Quais foram os filmes de Star Wars que. J.J Abrans dirigiu?': [
+        'Episódio 7 e Episódio 9',
+        'Episódio 7 e Espisodio 8',
+        'Episódio 2 e Espisodio 3',
+        'Episódio 4 e Espisodio 5',
+      ]
+    }, {
+      'Quais são os filhos de Anakin?': [
+        'Luke e Léia',
+        'Finn e Rey',
+        'Obi Wan e Rey',
+        'Padmé e Yoda',
+      ]
+    }, {
+      'Qual era o nome do dono de Anakin Skywalker?': [
+        'Watto',
+        'Wato',
+        'Jabba',
+        'Wotto',
+      ]
+    }, {
+      'Em qual planeta se passou a batalha final de ataque dos clones?': [
+        'Geonosis',
+        'Kamino',
+        'Coruscant',
+        'Mustafar',
+      ]
+    }, {
+      'Anakin acreditava que o Palpatine poderia o ajudar em algo, que foi o que fez ele trocar de lado. Em que Palpatine poderia ajudar Anakin?': [
+        'Salvando a vida de Padmé',
+        'Destruindo os jedis',
+        'Salvar sua mãe',
+        'Tornando ele um grande sith',
+      ]
+    }, {
+      'Em Star Wars Rogue One os rebeldes estão em busca de que?': [
+        'Planos de destruição da estrela da morte',
+        'Da princesa Léia',
+        'Um droide secreto',
+        'Mapa de localização de luke skywalker',
+      ]
+    }, {
+      'Em Star Wars Uma Nova Esperança a princesa Léia procura por um grande cavaleiro Jedi, quem é ele?': [
+        'Obi-Wan Kenobi',
+        'Qui-Gon Jinn',
+        'Yoda',
+        'Mace Windu',
+      ]
+    }, {
+      'Em Star Wars o império contra ataca Han Solo é congelado. Por que ele foi conjelado?': [
+        'Porque tinha uma enorme divida',
+        'Porque ele se sacrificou pelos outros',
+        'Porque era opositor ao império',
+        'Porque ele falava muito',
+      ]
+    }, {
+      'Em Star Wars O Retorno Jedi Palpatine é derrotado. Quem é responsável por isso?': [
+        'Darth Vader',
+        'Han Solo',
+        'Luke SkyWalker',
+        'Princesa Léia',
+      ]
+    }, {
+      'Em Star Wars o Despertar da Força descobrimos que Kylo Ren é filho de quem?': [
+        'Han Solo',
+        'Luke',
+        'Obi-Wan',
+        'Mace Windu',
+      ]
+    }, {
+      'Em Star Wars O ultimo Jedi como Luke morre?': [
+        'Se une a força',
+        'Fica muito velho',
+        'Com um tiro',
+        'É cortado por um sabre de luz',
+      ]
+    }, {
+      'Qual o nome da aprendiz de Anakin Skywalker?': [
+        'Ahsoka Tano',
+        'Assoka Thano',
+        'Obi-Wan',
+        'Luke Skywalker',
+      ]
+    }, {
+      'Quem leva Ahsoka Tano para a Ordem Jedi?': [
+        'Plo Koon',
+        'Anakin',
+        'Yoda',
+        'Obi-Wan',
+      ]
+    }, {
+      'Qual o nome do Jedi que autorizou a criação do exército de clones?': [
+        'Zaifo-Vias',
+        'Qui-Gon Jinn',
+        'Yoda',
+        'Mace Windu',
+      ]
+    }, {
+      'Quem destruiu o Templo Jedi e matou todas as crianças?': [
+        'Darth Vader',
+        'Ben Solo',
+        'Kylo Ren',
+        'Princesa Léia',
+      ]
+    }, {
+      'O que a Ordem 66 pretendia?': [
+        'Destruir todos os Jedi',
+        'Acabar com todos os Sith da Galáxia',
+        'Trair a Primeira Ordem',
+        'A paz entre Jedi e Sith',
+      ]
+    },    
+  ],
+};
